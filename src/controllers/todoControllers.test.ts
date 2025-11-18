02d1ddd33ecba62445a0f1a56617ebce2e5b57b7
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2025-11-19 00:05:09 +0530

    Update controller test file with get tasks function tests

--- src/controllers/todoControllers.test.ts
+++ src/controllers/todoControllers.test.ts
@@ -1,9 +1,10 @@
-import { addTask } from "./todoControllers";
-import { addTaskService } from "../services/todoService";
+import { addTask, getTasks } from "./todoControllers";
+import { addTaskService, getTasksService } from "../services/todoService";
 import { Request, Response } from "express";
 
 jest.mock("../services/todoService", () => ({
     addTaskService: jest.fn(),
+    getTasksService: jest.fn()
 }));
 
 const mockAddTaskService = addTaskService as jest.Mock;
@@ -53,6 +54,47 @@ describe("addTask Controller", () => {
         const errorMessage = "Failed to save task to the database.";
         mockAddTaskService.mockRejectedValue(new Error(errorMessage));
         await addTask(mockRequest as Request, mockResponse as Response);
+        expect(resStatus).toHaveBeenCalledWith(500);
+        expect(resJson).toHaveBeenCalledWith({ error: errorMessage });
+    });
+});
+const mockGetTasksService = getTasksService as jest.Mock;
+describe("getTasks Controller", () => {
+    let mockRequest: Partial<Request>;
+    let mockResponse: Partial<Response>;
+    let resStatus: jest.Mock;
+    let resJson: jest.Mock;
+    beforeEach(() => {
+        jest.clearAllMocks();
+        resStatus = jest.fn().mockReturnThis();
+        resJson = jest.fn().mockReturnThis();
+        mockRequest = {};
+        mockResponse = {
+            status: resStatus,
+            json: resJson,
+        };
+    });
+    it("should return 200 OK status and with array of tasks on success", async () => {
+        const mockTasks = [{ id: "id1", name: "Yoga" }, { id: "id2", name: "Atomic habits" }];
+        mockGetTasksService.mockResolvedValue(mockTasks);
+        await getTasks(mockRequest as Request, mockResponse as Response);
+        expect(mockGetTasksService).toHaveBeenCalledTimes(1);
+        expect(resStatus).toHaveBeenCalledWith(200);
+        expect(resJson).toHaveBeenCalledWith(mockTasks);
+    });
+    it("should return 200 OK status and an empty array if no tasks are found", async () => {
+        const mockTasks: any[] = [];
+        mockGetTasksService.mockResolvedValue(mockTasks);
+        await getTasks(mockRequest as Request, mockResponse as Response);
+        expect(mockGetTasksService).toHaveBeenCalledTimes(1);
+        expect(resStatus).toHaveBeenCalledWith(200);
+        expect(resJson).toHaveBeenCalledWith([]);
+    });
+    it("should return 500 Internal Server Error if the service fails", async () => {
+        const errorMessage = "Failed to retrieve tasks from the database.";
+        mockGetTasksService.mockRejectedValue(new Error(errorMessage));
+        await getTasks(mockRequest as Request, mockResponse as Response);
+        expect(mockGetTasksService).toHaveBeenCalledTimes(1);
         expect(resStatus).toHaveBeenCalledWith(500);
         expect(resJson).toHaveBeenCalledWith({ error: errorMessage });
     });
