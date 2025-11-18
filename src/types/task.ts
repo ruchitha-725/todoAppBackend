c80ee019cba28f6dba6897365d86de4c32281cad
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2025-11-18 19:34:59 +0530

    Update task type with addition of enums for status and priority

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,10 +1,20 @@
 export interface Task {
-    id?: string;
+    id?: string; 
     name: string;
     description: string;
-    status: string;
-    priority: string;
-    deadline: string;
-};
-
+    deadline: string; 
+    status: TaskStatus;
+    priority: TaskPriority;
+}
 export default Task;
+
+export enum TaskStatus {
+  PENDING = "Pending",
+  IN_PROGRESS = "In Progress",
+  COMPLETED = "Completed",
+}
+export enum TaskPriority {
+  LOW = "Low",
+  MEDIUM = "Medium",
+  HIGH = "High",
+}
