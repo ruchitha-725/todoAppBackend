6658a25dc2d6268c4dc6950cb94a34bd7dee6538
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2025-11-19 01:44:37 +0530

    Update route file with addition of delete task route path

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addTask, getTasks, updateTask } from "../controllers/todoControllers";
+import { addTask, getTasks, updateTask, deleteTask } from "../controllers/todoControllers";
 
 const todoRoutes = Router();
 
 todoRoutes.post("/add", addTask);
 todoRoutes.get("/all", getTasks);
 todoRoutes.put("/update/:id", updateTask)
+todoRoutes.delete("/delete/:id", deleteTask);
 
 export default todoRoutes;
