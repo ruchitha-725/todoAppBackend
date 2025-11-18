d92a8bf370cf772ded4e70d2c75e96d759fdac10
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2025-11-18 20:22:35 +0530

    Update controller with validations and handling errors

--- src/controllers/todoControllers.ts
+++ src/controllers/todoControllers.ts
@@ -8,13 +8,25 @@ const getErrorMessage = (error: unknown): string => {
     return String(error);
 };
 
-
 export const addTask = async (req: Request, res: Response) => {
     try {
         const task = await addTaskService(req.body);
         res.status(201).json(task);
     } catch (error) {
-        res.status(500).json({ error: getErrorMessage(error) });
+        const errorMessage = getErrorMessage(error);
+        if (errorMessage.startsWith("Missing required fields.") ||
+            errorMessage.includes("status must be one of") ||
+            errorMessage.includes("priority must be one of")) {
+            res.status(400).json({ error: errorMessage });
+        }
+        else if (errorMessage.includes("Task with name")) {
+            res.status(409).json({ error: errorMessage });
+        }
+        else if (errorMessage.startsWith("Failed to save task")) {
+            res.status(500).json({ error: errorMessage });
+        }
+        else {
+            res.status(500).json({ error: "An unexpected error occurred." });
+        }
     }
 };
-
